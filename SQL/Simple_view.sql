fe3e74fba875ee7f290c30b7919fe8b8dd73c5d6
Nilan19 <52948809+Nilan19@users.noreply.github.com>
2021-08-08 11:34:25 +1000

    Update Simple_view.sql

--- SQL/Simple_view.sql
+++ SQL/Simple_view.sql
@@ -1,6 +1,7 @@
 /* 5 Kyu Kata
 https://www.codewars.com/kata/5811527d9d278b242f000006/train/sql
-For this challenge you need to create a VIEW. This VIEW is used by a sales store to give out vouches to members who have spent over $1000 in departments that have brought in more than $10000 total ordered by the members id.
+For this challenge you need to create a VIEW. This VIEW is used by a sales store to give out vouches to members who have spent over $1000 in departments that 
+have brought in more than $10000 total ordered by the members id.
 The VIEW must be called members_approved_for_voucher then you must create a SELECT query using the view.
 
 */
